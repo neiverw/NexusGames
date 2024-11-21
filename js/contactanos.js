b5d37cc54f12007ce808147e8e9f5ded4f7650a6
Maximun23 <maximundilan@gmail.com>
2024-11-21 15:08:56 -0500

    cambios generales altern

--- js/contactanos.js
+++ js/contactanos.js
@@ -1,3 +1,19 @@
+function openNav() {
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+// Función para cerrar el menú
+function closeNav() {
+    document.getElementById("mobile-menu").style.width = "0";
+}
+
+// Cerrar el menú automáticamente al hacer clic en un enlace
+const menuLinks = document.querySelectorAll('#mobile-menu .overlay-content a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', closeNav);
+});
+
+
 /*contactanos*/
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
