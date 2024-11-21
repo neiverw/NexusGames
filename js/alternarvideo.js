bad97c8cd0b58ab0a67e9fbd0119b9063517c055
Maximun23 <maximundilan@gmail.com>
2024-11-21 15:44:54 -0500

    musica y reinicio del video

--- js/alternarvideo.js
+++ js/alternarvideo.js
@@ -6,14 +6,16 @@ const cameraLockedIcon = "imagenes/PepiconsPopCamera.png"; // Icono cuando la im
 
 cameraButton.addEventListener("click", () => {
   if (backgroundVideo.classList.contains("active")) {
-    // Mostrar imagen de fondo
+    // Mostrar imagen de fondo, detener y reiniciar el video
     backgroundVideo.classList.remove("active");
-    firstContentImg.style.backgroundImage =
-      'url("imagenes/spider2index.png")';
+    backgroundVideo.pause(); // Detiene el video
+    backgroundVideo.currentTime = 0; // Lo reinicia al inicio
+    firstContentImg.style.backgroundImage = 'url("imagenes/spider2index.png")';
     cameraButton.querySelector("img").src = cameraLockedIcon;
   } else {
-    // Mostrar video
+    // Mostrar video y reproducirlo
     backgroundVideo.classList.add("active");
+    backgroundVideo.play(); // Vuelve a reproducir el video
     firstContentImg.style.backgroundImage = "none";
     cameraButton.querySelector("img").src = cameraUnlockedIcon;
   }
