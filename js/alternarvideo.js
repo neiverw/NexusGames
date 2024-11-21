920d7edd28ca16cae82ec7896ed2174e5ea3f4e0
Maximun23 <maximundilan@gmail.com>
2024-11-21 14:18:00 -0500

    video_index_agregado

---
+++ js/alternarvideo.js
@@ -0,0 +1,20 @@
+const backgroundVideo = document.getElementById("background-video");
+const firstContentImg = document.getElementById("button1");
+const cameraButton = document.getElementById("cameraButton");
+const cameraUnlockedIcon = "../imagenes/PepiconsPopCameraOff.svg"; // Icono cuando el video está activo
+const cameraLockedIcon = "../imagenes/PepiconsPopCamera.svg"; // Icono cuando la imagen está activa
+
+cameraButton.addEventListener("click", () => {
+  if (backgroundVideo.classList.contains("active")) {
+    // Mostrar imagen de fondo
+    backgroundVideo.classList.remove("active");
+    firstContentImg.style.backgroundImage =
+      'url("../imagenes/spider2index.png")';
+    cameraButton.querySelector("img").src = cameraLockedIcon;
+  } else {
+    // Mostrar video
+    backgroundVideo.classList.add("active");
+    firstContentImg.style.backgroundImage = "none";
+    cameraButton.querySelector("img").src = cameraUnlockedIcon;
+  }
+});
