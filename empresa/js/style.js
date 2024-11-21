0f8ddeacc7709c69ba5fcc193ac7c43a6b94c962
neiverw <neiverwilchez25@gmail.com>
2024-11-20 19:08:53 -0500

    cambio producto1

--- empresa/js/style.js
+++ empresa/js/style.js
@@ -1,10 +1,17 @@
-function openNav(){
+function openNav() {
     document.getElementById("mobile-menu").style.width = "100%";
 }
 
-function closeNav(){
-    document.getElementById("mobile-menu").style.width = "0%";
+// Función para cerrar el menú
+function closeNav() {
+    document.getElementById("mobile-menu").style.width = "0";
 }
+
+// Cerrar el menú automáticamente al hacer clic en un enlace
+const menuLinks = document.querySelectorAll('#mobile-menu .overlay-content a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', closeNav);
+});
 /*contactanos*/
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
@@ -15,3 +22,4 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     // Resetear el formulario
     this.reset();
   });
+
